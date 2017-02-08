9418df03cda39d82e43dd13e54a7981666e24f95
Rafael De Jongh <info@rafaeldejongh.com>
2017-02-08 21:10:50 +0100

    Multiple Year Copyright Notice

--- footer.php
+++ footer.php
@@ -4,13 +4,12 @@
  * Contains the closing of the #content div and all content after.
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  * @package Rafael_De_Jongh
- * Dynamic Copyright Year: <?php echo date("Y"); ?>
  */
 ?>
 	</div><!-- #content -->
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			Copyright &copy; 2016 | Rafa&euml;l De Jongh | <a href="mailto:info@rafaeldejongh.com">info@rafaeldejongh.com</a> | All Rights Reserved.
+			Copyright &copy; 2016 &ndash; <?php echo date("Y"); ?> | Rafa&euml;l De Jongh | <a href="mailto:info@rafaeldejongh.com">info@rafaeldejongh.com</a> | All Rights Reserved
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
